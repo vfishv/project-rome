e537317b36b321ee4e944601b07b875d5c393583
Q <foldervapp@gmail.com>
2023-08-07 01:37:55 +0800

    m

---
+++ Android/samples/graphnotificationssample/app/src/main/java/com/microsoft/connecteddevices/graphnotifications/TestApp.java
@@ -0,0 +1,13 @@
+package com.microsoft.connecteddevices.graphnotifications;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class TestApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
